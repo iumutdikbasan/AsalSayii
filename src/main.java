79edfe8509551e35303837d407d5f970d5265298
rocenu <iumutdikbasan@gmail.com>
2022-08-30 16:28:43 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Eleman sayisini giriniz : ");
+        int n = input.nextInt();
+        int b=0, c=1,d;
+        for (int i=1;i<=n;i++){
+            d=b+c;
+            System.out.println(b+"+"+c+"="+d);
+            b=c;
+            c=d;
+
+            System.out.println(" ");
+        }
+
+
+
+
+
+
+    }
+}
